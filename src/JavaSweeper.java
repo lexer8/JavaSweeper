d5f7960318e568ef324f31b36491c99e378028bb
Lexer8 <lexer8@mail.ru>
2018-12-14 11:27:56 +0100

    Closing the Window and Stop the program.

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -5,4 +5,8 @@ public class JavaSweeper extends JFrame {
     public static void main(String[] args) {
         new JavaSweeper().setVisible(true);
     }
+
+    private JavaSweeper(){
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
 }

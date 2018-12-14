67371fd3577229fa9883c258da8eaa7200164acf
Lexer8 <lexer8@mail.ru>
2018-12-14 13:36:33 +0100

    There was added enum class Box.

---
+++ src/sweeper/Box.java
@@ -0,0 +1,19 @@
+package sweeper;
+
+public enum Box {
+    ZERO,
+    NUM1,
+    NUM2,
+    NUM3,
+    NUM4,
+    NUM5,
+    NUM6,
+    NUM7,
+    NUM8,
+
+    OPENED,
+    CLOSED,
+    FLAGED,
+    BOMBED,
+    NOBOMB
+}

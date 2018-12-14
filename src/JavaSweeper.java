37b7feca517b3b65e7977a92c5d9908872e10a48
Lexer8 <lexer8@mail.ru>
2018-12-14 12:00:23 +0100

    Refactoring JavaSweeper() constructor. There were added 2 methods: initPanel and initFrame.

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -3,16 +3,24 @@ import java.awt.*;
 
 public class JavaSweeper extends JFrame {
 
-    JPanel panel;
+    private JPanel panel;
 
     public static void main(String[] args) {
         new JavaSweeper();
     }
 
     private JavaSweeper() {
+        initPanel();
+        initFrame();
+    }
+
+    private void initPanel() {
         panel = new JPanel();
         panel.setPreferredSize(new Dimension(500,300));
         add(panel);
+    }
+
+    private void initFrame() {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Java Sweeper");
         setVisible(true);

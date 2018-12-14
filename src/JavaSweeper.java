0e938a0382d5bae0f1918a7f621513a9eb5d9bfd
Lexer8 <lexer8@mail.ru>
2018-12-14 11:34:15 +0100

    Set Window properties: location, title.

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -3,10 +3,14 @@ import javax.swing.*;
 public class JavaSweeper extends JFrame {
 
     public static void main(String[] args) {
-        new JavaSweeper().setVisible(true);
+        new JavaSweeper();
     }
 
     private JavaSweeper(){
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Java Sweeper");
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setResizable(false);
     }
 }

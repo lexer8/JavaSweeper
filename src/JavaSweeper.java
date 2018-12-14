54ca18f379394ee3166c19f66264b299e02e796b
Lexer8 <lexer8@mail.ru>
2018-12-14 11:51:43 +0100

    Add panel to window

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -1,16 +1,23 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class JavaSweeper extends JFrame {
+
+    JPanel panel;
 
     public static void main(String[] args) {
         new JavaSweeper();
     }
 
-    private JavaSweeper(){
+    private JavaSweeper() {
+        panel = new JPanel();
+        panel.setPreferredSize(new Dimension(500,300));
+        add(panel);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Java Sweeper");
-        setLocationRelativeTo(null);
         setVisible(true);
         setResizable(false);
+        pack();
+        setLocationRelativeTo(null);
     }
 }

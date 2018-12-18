d0d6b47ce725f937c924b79b5798917f8c05813d
Lexer8 <lexer8@mail.ru>
2018-12-18 09:49:42 +0100

    Class Range was added.

---
+++ src/sweeper/Ranges.java
@@ -0,0 +1,19 @@
+package sweeper;
+
+public class Ranges {
+
+    private static Coord size;
+
+    static void setSize(Coord size) {
+        Ranges.size = size;
+    }
+
+    public static void setSize(int cols, int rows) {
+        Coord size = new Coord(cols, rows);
+        setSize(size);
+    }
+
+    static Coord getSize() {
+        return size;
+    }
+}
